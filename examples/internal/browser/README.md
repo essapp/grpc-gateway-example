9a16fb63e78fd39adf0ec7fb121f8dd596f075c1
ess <ess@localhost.localdomain>
2025-12-24 18:10:02 +0800

    init

---
+++ examples/internal/browser/README.md
@@ -0,0 +1,12 @@
+# Browser example
+
+This directory contains an example use of gRPC-Gateway with web browsers.
+The following commands automatically runs integration tests with phantomjs.
+
+```shell-session
+$ pnpm add -g gulp-cli
+$ pnpm install
+$ gulp
+```
+
+then, open `http://localhost:8000` with your browser.
---
+++ examples/internal/browser/gulpfile.js
@@ -0,0 +1,71 @@
+const { exec, spawn } = require('child_process');
+const { parallel, series } = require('gulp');
+const { join } = require('path');
+const { stat, readFile } = require('fs/promises');
+const { H3, handleCors, serve, serveStatic } = require('h3');
+
+const buildGS = () => {
+  const cmd = 'go build -o bin/example-server github.com/grpc-ecosystem/grpc-gateway/v2/examples/internal/cmd/example-grpc-server';
+  return exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Clean failed: ${error}`);
+    } else {
+      console.log('build grpc server success');
+    }
+  });
+}
+
+const buildGW = () => {
+  const cmd = 'go build -o bin/example-gw github.com/grpc-ecosystem/grpc-gateway/v2/examples/internal/cmd/example-gateway-server';
+  return exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Clean failed: ${error}`);
+    } else {
+      console.log('build gw server success');
+    }
+  });
+}
+
+const runGS = (done) => {
+  const gs = spawn('bin/example-server', [], { stdio: 'inherit' });
+  process.on('exit', () => {
+    gs.kill();
+  });
+  return done();
+}
+
+const runGW = (done) => {
+  const gw = spawn('bin/example-gw', ['--openapi_dir', join(__dirname, "../proto/examplepb"),], { stdio: 'inherit' });
+  process.on('exit', () => {
+    gw.kill();
+  });
+  return done();
+}
+
+const server = (done) => {
+  const app = new H3({ debug: true });
+  serve(
+    app.all('/**', (event) => {
+      if (handleCors(event, { origin: "*" })) {
+        return;
+      }
+      return serveStatic(event, {
+        indexNames: ['/index.html'],
+        getContents: (id) => readFile(join('public', id)),
+        getMeta: async (id) => {
+          const stats = await stat(join('public', id)).catch(() => { });
+          if (stats?.isFile()) {
+            return {
+              size: stats.size,
+              mtime: stats.mtimeMs,
+            };
+          }
+        },
+      });
+    }
+    )
+  );
+  return done();
+}
+
+exports.default = series(parallel(buildGS, buildGW), runGS, runGW, server);
